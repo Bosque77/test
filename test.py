8122707506097a7ff977b55f655b821ba3da83a5
Forest <forestschwrtz@gmail.com>
2022-06-10 22:50:37 -0700

    Revert "defined functions"
    This reverts commit 238b5619db02d835aaf6e42dfa3fb50833ab2951.

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 def subtract(x,y):
     return x-y
+=======
+def subtract():
+    pass
+>>>>>>> parent of 238b561... defined functions
 
 def add():
-    return x+y
+    pass
 
 =======
 >>>>>>> parent of 58b4ff1... defined subtract function
