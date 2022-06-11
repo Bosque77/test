a9dba8ba8803a8bd7a8d021d8aa9111083046ea2
Forest <forestschwrtz@gmail.com>
2022-06-11 14:58:55 -0700

    updated functions

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 def subtract(x,y):
     return x-y
-=======
-def subtract():
-    pass
->>>>>>> parent of 238b561... defined functions
 
 def add():
     pass
-
-=======
->>>>>>> parent of 58b4ff1... defined subtract function
